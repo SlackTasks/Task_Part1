b20c60e3eb0099f5b526ec4dcaf72d40285fc505
magava <magaava@gmail.com>
2021-01-07 01:11:27 +0400

    Task1_1: Removed Array.isArray checking.

--- Task1_1.js
+++ Task1_1.js
@@ -1,19 +1,18 @@
-function numberOfMax(arg){
-	if (Array.isArray(arg)) {
-    	var max = arg[0];
-        var numOfMax = 0;
-        for (var i = 1; i < arg.length; i++) {
-        	if (arg[i] > max) {
-            	max = arg[i];
-                numOfMax = 0;
-            }
-            
-            if (arg[i] === max) {
-            	numOfMax++;
-            }
-            
+function numberOfMax(arg) {
+
+    var max = arg[0];
+    var numOfMax = 0;
+    for (var i = 1; i < arg.length; i++) {
+        if (arg[i] > max) {
+            max = arg[i];
+            numOfMax = 0;
         }
+
+        if (arg[i] === max) {
+            numOfMax++;
+        }
+
     }
-    
+
     return numOfMax;
 }
