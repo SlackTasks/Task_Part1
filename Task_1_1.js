666102dcdfdffe6dccf9c8df05f2c745a23e0b1a
HOVO1997 <hsanosyan@bk.ru>
2021-01-06 23:36:51 +0400

    first_task

---
+++ Task_1_1.js
@@ -0,0 +1,66 @@
+///// 1
+const max = arg => arg.reduce((a, b) => {
+    const m = Math.max(...arg)
+    if (m === b) a++
+    return a
+}, 0)
+
+console.log(max([1, 2, 8, 8, 8, 8, 8, 3, 4, 4]))
+
+//// 2
+const min = arg => arg.sort((a, b) => a - b)
+
+console.log(min([1,5,8,2,3]))
+
+//// 3 - a PAHANJ@ CHASKACA :(
+
+// /// 3 - b
+const fn = (arr) => {
+    let newArr = []
+    nextPrime:
+        for (let i = 0; i <= arr.length-1; i++) {
+            if(arr[i] >= 2){
+                for (let j = 2; j < i; j++) {
+                    if (arr[i] % j === 0) continue nextPrime;
+                }
+                newArr.push(arr[i])
+            }
+        }
+   return newArr
+}
+
+const val = fn([1,2,3,4,5,6,7,8,9,10,11,12])
+
+const multi = val => {
+    let acc = val[0]
+    for (let i = 1; i < val.length; i++) {
+       acc *= val[i]
+    }
+    return acc
+}
+
+console.log(multi(val))
+
+//// 3 - g
+
+function reply(arg) {
+    const unique = [...new Set(arg)]
+    let count = 0
+    for (let i = 0; i < unique.length; i++) {
+        for (let j = 0; j < arg.length; j++) {
+            if(unique[i] === arg[j]){
+                count ++
+            }
+        }
+        console.log(`${unique[i]}-${count}`)
+        count = 0
+    }
+}
+
+console.log(reply([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4, 4]))
+
+
+
+
+
+
