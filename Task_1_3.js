90124be2fe7f7290ff1b38b3f5c897ca7ec08703
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-01-07 14:21:10 +0300

    complete task1_3

--- Task_1_3.js
+++ Task_1_3.js
@@ -21,3 +21,43 @@ function sum(arr, c) {
 }
 let arr = [145, 152, 172, 253];
 console.log(sum(arr, 10));
+
+/* 	բ) պարզ թվերի արտադրյալը */
+
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function multPrimeNumbers(arr) {
+  let result = 1;
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    if (isPrime(num)) {
+      result *= num;
+    }
+  }
+  return result;
+}
+
+let arr = [5, 11, 6, 4];
+console.log(multPrimeNumbers(arr));
+
+/* գ) կրկնվող տարրերի քանակը */
+
+function countRepeatitions(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) count++;
+    }
+  }
+  return count;
+}
+
+let arr = [5, 11, 6, 4, 5, 7, 11, 15, 15];
+console.log(countRepeatitions(arr));
