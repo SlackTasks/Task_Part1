f18f108942fbf611268c8d74a4f4508daa0ec046
ArMinasyan <vip.arman.minasyan.197@gmail.com>
2021-01-07 19:53:10 +0400

    Lava

--- Task_1_h.js
+++ Task_1_h.js
@@ -1,4 +1,6 @@
-var arr = [1, 2, 3, 4, 4, 2];
+///var mi kirari, let kam const miayn
+
+var arr = [1, 2, 3, -4, -4, 3, 2];
 var max = arr[0];
 var count = 0;
 
@@ -8,9 +10,9 @@ for (var k = 0; k < arr.length; k++) {
   }
 }
 console.log("Max is: " + max);
-for(var j = 0 ;j<arr.length; j++){
-	if(arr[j]==max){
-		count++
-	}
+for (var j = 0; j < arr.length; j++) {
+  if (arr[j] == max) {
+    count++
+  }
 }
-console.log(count)
+console.log("Count is: " + count)
