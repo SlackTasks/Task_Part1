e009a062086711b0c210895e9f5dceadc892489b
magava <magaava@gmail.com>
2021-01-09 02:34:16 +0400

    Task1_3. Added functions for parts b and c.

--- Task1_3.js
+++ Task1_3.js
@@ -48,3 +48,76 @@ function numOfElementsWithSumOfDigits(arr, num) {
 
     return qty;
 }
+
+
+function isPrime(num) {
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }        
+    }
+
+    return true;
+}
+
+
+function productOfPrimeElements(arr) {
+    let prod = 1;
+    for (let i = 0; i < arr.length; i++) {
+        if (isPrime(arr[i])) { 
+            prod *= arr[i];
+        }
+        
+    }
+
+    return prod;
+}
+
+
+function sortDesc(arg) {
+
+    for (var i = 0; i < arg.length - 1; i++) {
+
+        for (var j = i + 1; j < arg.length; j++) {
+
+            if (arg[i] < arg[j]) {
+                var tmp = arg[i];
+                arg[i] = arg[j];
+                arg[j] = tmp;
+            }
+
+        }
+
+    }
+    return arg;
+}
+
+
+
+function printNumberOfRepeatingElements(arr) {
+
+    let sortedArr = sortDesc(arr);
+    let count = 1;
+
+    let i = 0; 
+    let j = 1;
+ 
+    while (i < sortedArr.length-1) {
+        j = i + 1;
+        elem = sortedArr[i];
+
+        while (sortedArr[j] == sortedArr[i]) {
+            count++;
+            j++;
+        }
+
+        if (count > 1) {
+            console.log(sortedArr[i] + " is repeated " + count + " times. ");           
+        }
+
+        count = 1;
+        i = j;
+    }
+
+}
