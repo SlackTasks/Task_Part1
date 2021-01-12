a4fd0074747d08aa27b7fac0a7290f0d4d46672d
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-01-12 18:48:21 +0300

    adding users

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
 let data = [
   {
-    id: 1,
     fullName: "John Doe",
     email: "john@98gmail.com",
   },
   {
-    id: 2,
     fullName: "James Smith",
     email: "james@98gmail.com",
   },
   {
-    id: 3,
     fullName: "Linda Gomes",
     email: "lindan@98gmail.com",
   },
   {
-    id: 4,
     fullName: "Harry Li",
     email: "harry@98gmail.com",
   },
   {
-    id: 5,
     fullName: "Alison Parker",
     email: "alison@98gmail.com",
   },
   {
-    id: 6,
     fullName: "Sara Parker",
     email: "sara@98gmail.com",
   },
@@ -35,13 +29,36 @@ window.onload = () => {
   loadTableData(data);
 };
 
+function idGenerator() {
+  return Math.random().toString(32).slice(2);
+}
 function loadTableData(data) {
   const tableBody = document.getElementById("tableData");
   let htmlData = "";
   for (let user of data) {
+    user.id = idGenerator();
     htmlData += `<tr><td>${user.id}</td><td>${user.fullName}</td><td>${user.email}</td><td>
   <i class="fas fa-edit"></i><i class="fas fa-trash-alt"></i><input type="checkbox" /> 
     </td></tr>`;
   }
   tableBody.innerHTML = htmlData;
 }
+
+function addUser() {
+  let email = document.querySelector(".email").value;
+  let name = document.querySelector(".name").value;
+  let surname = document.querySelector(".surname").value;
+  let inputs = document.querySelectorAll(".inputFields input");
+  if (name && surname && email) {
+    let fullName = `${name} ${surname}`;
+    let user = {
+      id: idGenerator(),
+      fullName,
+      email,
+    };
+    data.push(user);
+    inputs.forEach((input) => (input.value = ""));
+    loadTableData(data);
+  }
+}
+document.querySelector(".addButton").addEventListener("click", addUser);
